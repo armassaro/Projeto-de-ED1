7edf0855cfd578471f83f35a99dd0bc6658a4f92
MichelRochytor <michelrochytor.of@gmail.com>
2023-11-01 09:55:15 -0300

    agora

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Funcionarios{
+    char nome[40];
+}Funcionarios;
+
+typedef struct No{
+    Funcionarios funcionarios;
+    struct Lista *proximo, *anterior;
+}No;
+
+typedef struct Lista{
+    No* Inicio;
+}Lista;
+
 int main (){
+    Lista lista;
+
     return 0;
 }
