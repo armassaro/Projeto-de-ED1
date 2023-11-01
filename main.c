264eef23e0895e5ccb67982d63af5fb2178b5da4
MichelRochytor <michelrochytor.of@gmail.com>
2023-11-01 10:15:24 -0300

    oficial1

--- main.c
+++ main.c
@@ -12,10 +12,16 @@ typedef struct No{
 
 typedef struct Lista{
     No* Inicio;
+    int tamanhoLista;
 }Lista;
+
+void inicializaLista(Lista* lista){
+    lista->Inicio = NULL;
+    lista->tamanhoLista = 0;
+}
 
 int main (){
     Lista lista;
-
+    inicializaLista(&lista);
     return 0;
 }
